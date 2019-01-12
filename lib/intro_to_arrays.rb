6b15227e0d233a1eb1a4a8702c7a01ad8387d13e
Sharon Mun <sharonymun@gmail.com>
2019-01-12 17:58:56 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,32 @@
+def instantiate_new_array
+  my_new_array = []
+  my_new_array
+end
+
+def array_with_two_elements
+  my_two_array = [" ", " "]
+end
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(third)
+  third[2]
+end
+
+def last_element(last)
+  last.last
+end
+
+def first_element_with_array_methods(array)
+  array.first
+end
+
+def last_element_with_array_methods(array)
+  array.last
+end
+
+def length_of_array(array)
+  array.length
+end
